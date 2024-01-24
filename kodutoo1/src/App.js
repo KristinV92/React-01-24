470918caa5c2a67191a0a6778b7fc1ef078895b3
KristinV92 <kristin.lapsanit@gmail.com>
2024-01-24 12:58:54 +0200

    kodutoo1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <button className="nupp" >Vajuta</button>
+ <p className="Tere">Tere</p>
+ <p className="hommikust" >hommikust</p>
+ <img className="pilt" src="https://static.displate.com/280x392/displate/2023-03-26/adce19452f4c36d773e1dd3ed5b1d416_ad3fdc41398e3cb8ad2ed2e5647f50cd.jpg" alt="koer"></img>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/tNnY7fyoZMQ" alt="tutvustus"></iframe>
+
+    </div>
+  
+  );
+}
+
+export default App;
