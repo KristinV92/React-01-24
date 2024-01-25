56fef5e228449ebde415875e912a177ba7941faf
KristinV92 <kristin.lapsanit@gmail.com>
2024-01-25 11:48:06 +0200

    Uuendus

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -6,9 +6,46 @@ function App() {
     <div className="App">
  <button className="nupp" >Vajuta</button>
  <p className="Tere">Tere</p>
- <p className="hommikust" >hommikust</p>
+ <span className="hommikust" >hommikust</span><br></br>
  <img className="pilt" src="https://static.displate.com/280x392/displate/2023-03-26/adce19452f4c36d773e1dd3ed5b1d416_ad3fdc41398e3cb8ad2ed2e5647f50cd.jpg" alt="koer"></img>
-<iframe width="420" height="315" src="https://www.youtube.com/embed/tNnY7fyoZMQ" alt="tutvustus"></iframe>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/tNnY7fyoZMQ" alt="tutvustus" title="koera video"></iframe>
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
 
     </div>
   
