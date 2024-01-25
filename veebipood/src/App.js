252fb70ab7113b9ffda57017fc5d87e3b1fbd2f1
KristinV92 <kristin.lapsanit@gmail.com>
2024-01-25 11:48:51 +0200

    html, css tekst

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="Nobe elektriauto"></img>
      <button className="nupp">Vajuta</button>
+     <span>Elektriauto</span>
+     <div className="Hübriid">Hübriid</div>
+     <h3>Pealkiri</h3>
     </div>
   );
 }
