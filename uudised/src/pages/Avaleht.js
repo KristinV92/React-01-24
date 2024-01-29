18bc12abd094f1cec10d58764d859feb2e8b6278
KristinV92 <kristin.lapsanit@gmail.com>
2024-01-29 19:35:25 +0200

    kodutoo 2-3

---
+++ uudised/src/pages/Avaleht.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Avaleht() {
+  return (<div> <div>See on avaleht, nähtav localhost:3000 aadressil</div>
+  <img src="https://hative.com/wp-ontent/uploads/2013/08/bark-news-media-logo.png" alt="" />
+   </div> );
+}
+
+export default Avaleht;
