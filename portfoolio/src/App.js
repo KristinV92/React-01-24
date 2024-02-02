13cbfb27d8979ead1a23fe49a0cce6643a7863b0
KristinV92 <kristin.lapsanit@gmail.com>
2024-02-02 18:48:24 +0200

    start

---
+++ portfoolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <img className="home-page" src="abstract.jpg" alt="" />
+    <div className="line" ></div>
+
+    <img src="/work.png" alt="" /> 
+    <img src="/hobby.jpg" alt="" /> 
+    <img src="/courses.jpg" alt="" /> 
+   
+    </div>
+  );
+}
+
+export default App;
