d3040ab95990b7f7cef410a38eb35ea49127a6a6
KristinV92 <kristin.lapsanit@gmail.com>
2024-02-03 21:36:09 +0200

    Blog youtube

---
+++ blog/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import Header from "../../header/Header"
+
+function Home() {
+  return (
+    <div className="home">
+        <Header/>
+        homepage
+    </div>
+  )
+}
+
+export default Home
---
+++ blog/src/pages/home/header/Header.js
@@ -0,0 +1,9 @@
+import "./home.css"
+
+function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
