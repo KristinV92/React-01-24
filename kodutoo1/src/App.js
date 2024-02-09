922d7ba50c445ef82d9bf3f9a010df94386513e2
KristinV92 <kristin.lapsanit@gmail.com>
2024-02-09 19:37:20 +0200

    useState Ref

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -1,52 +1,73 @@
-//import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Avaleht from './pages/Avaleht';
+import Kontakt from './pages/Kontakt';
+import Meist from './pages/Meist';
+import Seaded from './pages/Seaded';
+import { useState } from 'react';
+import { useRef } from 'react';
+import Leht from './pages/Leht';
+import Loader from './pages/Loader';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123") {
+    muudaSisselogitud("jah");
+    muudaSonum(kasutajaNimiRef.current.value + ", oled sisselogitud");
+  } else {
+    muudaSonum("Vale parool");
+  }
+  }
+  const logiValja = () => {
+    muudaSisselogitud("ei");
+    muudaSonum("");
+  }
+
   return (
     <div className="App">
- <button className="nupp" >Vajuta</button>
- <p className="Tere">Tere</p>
- <span className="hommikust" >hommikust</span><br></br>
- <img className="pilt" src="https://static.displate.com/280x392/displate/2023-03-26/adce19452f4c36d773e1dd3ed5b1d416_ad3fdc41398e3cb8ad2ed2e5647f50cd.jpg" alt="koer"></img>
-<iframe width="420" height="315" src="https://www.youtube.com/embed/tNnY7fyoZMQ" alt="tutvustus" title="koera video"></iframe>
-<table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
+      <div>{sonum}</div>
+      { sisselogitud === "ei" && <div>
+        <label>Kasutajanimi</label> <br />
+        <input ref={kasutajaNimiRef} type="text" /><br />
+        <label>Parool</label> <br />
+        <input ref={paroolRef} type="password" /><br />
+      </div>}
 
+      { sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+      { sisselogitud === "jah" && <button onClick={logiValja}>Logi välja</button>}
+ 
+      <Link to="/">
+          <button>Avaleht</button>
+      </Link>
+      <Link to="/kontakt">
+          <button>Kontakt</button>
+      </Link>
+      <Link to="/meist">
+          <button>Meist</button>
+      </Link>
+      <Link to="/seaded">
+          <button>Seadetesse</button>
+      </Link>
+      <Link to="leht">
+        <button>Leht</button>
+      </Link>
+      <Link to="loader">
+        <button>Loader</button>
+      </Link>
+
+      <Routes>
+        <Route path='' element={ <Avaleht />} />
+        <Route path='kontakt' element={ <Kontakt /> } />
+        <Route path='meist' element={ <Meist /> } />
+        <Route path='seaded' element={ <Seaded /> } />
+        <Route path='leht' element= { <Leht /> } />
+        <Route path='loader' element= { <Loader /> } />
+      </Routes>
     </div>
   
   );
