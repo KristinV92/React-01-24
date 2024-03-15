6a8eda59cc18c2ae9f6ddfe128bb6b3c736e33c5
KristinV92 <kristin.lapsanit@gmail.com>
2024-03-15 19:54:31 +0200

    e-mail, map, cart, routes, pages

---
+++ webshop/src/pages/global/Cart.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import productsFromFile from "../../data/products.json"
+
+function Cart() {
+  const [cart, updateCart] = useState(productsFromFile);
+
+  const deleteFromCart =(index) => {
+    cart.splice(index, 1);
+    updateCart(cart.slice());
+   }
+   
+   const addToCart= (newProduct) => {
+     cart.push(newProduct);
+     updateCart(cart.slice());
+   }
+   
+   const calculateTotal = () => {
+     let amount = 0;
+     cart.forEach(product => amount = amount + product.price);
+     return amount;
+   }
+
+  return (
+    <div>
+        {cart.length === 0 && <div>The shopping cart is currently empty</div>}
+  
+
+      {cart.length > 0 &&
+      <div>
+        <div>You have {cart.length} items in your cart</div>
+        <button onClick={() => updateCart([])}>Empty the shopping cart</button>
+      </div>}
+
+        {cart.map((product, index) => 
+          <div key={index}>
+            {index}.
+            <img style={{width: "50px"}} src={product.image} alt="" />
+            {product.title} - {product.price} €
+            <button onClick={() => deleteFromCart(index)}>Delete</button> 
+            <button onClick={() => addToCart(product)}>Add one to the end</button> 
+          </div> )}
+
+          <div>Total cost: {calculateTotal()}€</div>
+    </div>
+  )
+}
+
+export default Cart
---
+++ webshop/src/pages/global/ContactUs.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_pfc8k1n', 'template_1swcbd9', form.current, {
+        publicKey: '9tqQ3DoGU5Go56ugW',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="from_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
