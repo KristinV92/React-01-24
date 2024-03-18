9d20215eb170837bc9063a290068dce32553700f
KristinV92 <kristin.lapsanit@gmail.com>
2024-03-18 13:08:32 +0200

    objekt objektis,API päringud, array loxalStoragesse

--- webshop/src/pages/global/Cart.jsx
+++ webshop/src/pages/global/Cart.jsx
@@ -1,22 +1,45 @@
-import React, { useState } from 'react'
-import productsFromFile from "../../data/products.json"
+import React, { useEffect, useState } from 'react'
+// import cartFromFile from "../../data/cart.json"
 
-function Cart() {
-  const [cart, updateCart] = useState(productsFromFile);
+function Cart() {           //1. ja 2. samm
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || []);
+  const [parcelMachines, setParcelMachines] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.omniva.ee/locations.json")
+    .then(res => res.json())
+    .then(body => setParcelMachines(body));
+  }, []);
 
   const deleteFromCart =(index) => {
-    cart.splice(index, 1);
-    updateCart(cart.slice());
+    cart.splice(index, 1); // 3.samm
+    setCart(cart.slice()); //uuendab HTMLi  
+    localStorage.setItem("cart", JSON.stringify(cart)); //4. ja 5. salvestab localStorage-sse
    }
    
-   const addToCart= (newProduct) => {
-     cart.push(newProduct);
-     updateCart(cart.slice());
-   }
+  const decreaseQuantity = (index) => {
+    cart[index].kogus = cart[index].kogus - 1; // 3.samm
+    if (cart[index].kogus === 0) {
+      cart.splice(index, 1); // 3.samm
+    }
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart)); //4. ja 5. salvestab localStorage-sse
+  }
+
+  const increaseQuantity = (index) => {
+    cart[index].kogus = cart[index].kogus + 1;  // 3.samm
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart)); //4. ja 5. salvestab localStorage-sse
+  }
+
+  //  const addToCart= (newProduct) => {
+  //    cart.push(newProduct);
+  //    updateCart(cart.slice());
+  //  }
    
    const calculateTotal = () => {
      let amount = 0;
-     cart.forEach(product => amount = amount + product.price);
+     cart.forEach(product => amount = amount + product.toode.price * product.kogus);
      return amount;
    }
 
@@ -29,17 +52,34 @@ function Cart() {
       {cart.length > 0 &&
       <div>
         <div>You have {cart.length} items in your cart</div>
-        <button onClick={() => updateCart([])}>Empty the shopping cart</button>
+        <button onClick={() => setCart([])}>Empty the shopping cart</button>
       </div>}
 
         {cart.map((product, index) => 
           <div key={index}>
-            {index}.
-            <img style={{width: "50px"}} src={product.image} alt="" />
-            {product.title} - {product.price} €
+            {index+1}.
+            <img style={{width: "50px"}} src={product.toode.image} alt="" />
+            {product.toode.title} - {product.toode.price} €
+            {/* 
+            kuvage KODUS rating välja
+            pange kogusumma kõrvale toodete keskmine reiting:
+            reitingute kogusumma jagatud koguarv
+            ärge arvestage koguseid. pluus 6tk, kell 3tk
+             */}
+            <button onClick={() => decreaseQuantity(index)}>-</button>
+            <div>{product.kogus} pc</div>
+            <button onClick={() => increaseQuantity(index)}>+</button>
+            <div>{product.toode.price * product.kogus} €</div>
             <button onClick={() => deleteFromCart(index)}>Delete</button> 
-            <button onClick={() => addToCart(product)}>Add one to the end</button> 
+            {/* <button onClick={() => addToCart(product)}>Add one to the end</button>  */}
           </div> )}
+
+
+          <select>
+           {parcelMachines.map(pm => <option>{pm.NAME}</option>)} 
+          </select>
+
+
     </div>
   )
 }
--- webshop/src/pages/global/HomePage.jsx
+++ webshop/src/pages/global/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import cartFromFile from "../../data/cart.json";
 import productsFromFile from "../../data/products.json";
 import { Link } from 'react-router-dom';
 
@@ -6,14 +7,34 @@ function HomePage() {
     const [products, setProducts] = useState(productsFromFile);
 
     const addCart = (product) => {
-      productsFromFile.push(product);
+      const cartLS = JSON.parse(localStorage.getItem("cart")) || [];
+
+      const index = cartLS.findIndex(cp => cp.toode.id === product.id);
+      if (index >= 0) { //!== -1 kui ei leitud array seest, siis index on -1
+        //kinkekaartidel: kogus = kogus + 1
+        cartLS[index].kogus = cartLS[index].kogus + 1;
+        // cartFromFile[index].kogus += 1; iseendale liidetakse 1
+        // cartFromFile[index].kogus++; kasutatakse tsüklites
+      } else {
+        cartLS.push({"kogus": 1, "toode": product});
+      }
+      localStorage.setItem("cart", JSON.stringify(cartLS));
+
+      // parem klõps -> inspect -> application
+      // Array localStorage-sse salvestamiseks:
+      // 1. Võtta localStorage-st: localStorage.getItem()    et saada kätte vana seis
+      // 2. Võtta jutumärgid maha: JSON.parse()       localStorage annab jutumärkides, aga 
+      //      meie tahame .findIndex() ja .push() teha - seda saab teha array'del
+      // 3. Muudame/ lisame array'le: .push()    
+      // 4. Lisame jutumärgid tagasi: JSON.stringify()
+      // 5. Lisame localStorage-sse tagasi:   localStorage.setItem()   et uueneks brauseris
     }
 
   return (
     <div>
       {products.map((product, index) =>
           <div key={product.id}>
-              <img style={{width: "100px"}} src="{product.image}" alt="" />
+              <img style={{width: "100px"}} src={product.image} alt="" />
               <div>{product.title}</div>
               <div>{product.price}€</div>
 
