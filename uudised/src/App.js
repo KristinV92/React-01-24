e1755d57c132609702cea4601a6fa9da904e5d39
KristinV92 <kristin.lapsanit@gmail.com>
2024-03-23 13:20:02 +0200

    API

--- uudised/src/App.js
+++ uudised/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom';
 import Avaleht from './pages/Avaleht';
 import Uudised from './pages/Uudised';
 import Kontakt from './pages/Kontakt';
@@ -8,6 +8,8 @@ import LisaUudis from './pages/LisaUudis';
 import HaldaUudiseid from './pages/HaldaUudiseid';
 import YksUudis from './pages/YksUudis';
 import MuudaUudis from './pages/MuudaUudis';
+import KasutajaPostitus from './pages/KasutajaPostitus';
+import YksPostitus from './pages/YksPostitus';
 
 function App() {
   return (
@@ -40,6 +42,8 @@ function App() {
   <Route path='halda' element= {<HaldaUudiseid />} />
   <Route path='uudis/:index' element= {<YksUudis />} />
   <Route path='muuda/:index' element= {<MuudaUudis />} />
+  <Route path='kasutaja-postitus/:kasutajaId' element= {<KasutajaPostitus />} />
+  <Route path='vaata-postitus/:postituseId' element= {<YksPostitus />} />
 </Routes>
     </div>
   );
